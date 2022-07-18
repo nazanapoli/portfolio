0a87f21eb56b1105bf4ca1b7a2fdf322498486f0
Naza Napoli <nazanapoli@icloud.com>
2022-07-18 17:33:12 -0300

    cambio de links

--- js/index.js
+++ js/index.js
@@ -1,21 +1,9 @@
-document.getElementById('jsVelas').addEventListener('click',()=>{
-    window.location.href = "https://no-me-olvides.netlify.app/index.html";
-});
-document.getElementById('appTareas').addEventListener('click',()=>{
-    window.location.href = "https://tareas-app-site.netlify.app/";
-});
-document.getElementById('APIClima').addEventListener('click',()=>{
-    window.location.href = "https://web-clima-napoli.netlify.app/";
-});
-document.getElementById('bungeCSS').addEventListener('click',()=>{
-    window.location.href = "https://bio-bunge.vercel.app/";
-});
 document.getElementById('telefono').addEventListener('click',()=>{
     contTel++
     if(contTel<=1){
         let num = document.createElement('p')
         num.classList.add('bold')
-        num.innerText=`+54 9 1123905967`
+        num.innerText=`+54 9 11 23905967`
         telefono.append(num)
     }
 });
